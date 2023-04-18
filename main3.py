e8cc723a9db4ed58511ed6e3c0310eafb43c2a48
MaksimCh1988 <maksim.ch@yahoo.com>
2023-04-18 19:44:49 +0300

    3.9 Магические методы __iter__ и __next__ и 4.5 Делегирование в Python

--- main3.py
+++ main3.py
@@ -13,3 +13,31 @@ class PowerTwo():
             index = self.counter
             self.counter += 1
             return 2 ** index
+
+######################################
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name, self.mileage, self.capacity = name, mileage, capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+    def display(self):
+        print(f'Total {self.name} fare is: {self.fare()}')
+
+
+class Bus(Vehicle):
+    def __init__(self, name, mileage):
+        super().__init__(name, mileage, capacity=50)
+
+    def fare(self):
+        return super().fare() * 1.1
+
+
+class Taxi(Vehicle):
+    def __init__(self, name, mileage):
+        super().__init__(name, mileage, capacity=4)
+
+    def fare(self):
+        return super().fare() * 1.35
